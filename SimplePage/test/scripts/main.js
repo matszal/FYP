04ffebe4b7f14dfd443bf6d8ca5721bee640cb9e
matszal <mszalkowski2011@gmail.com>
2017-10-29 00:32:28 +0100

    added some JS

--- SimplePage/test/scripts/main.js
+++ SimplePage/test/scripts/main.js
@@ -1,6 +1,23 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'AWS Based ecommerce platform';
 
+
+
+function setUserName(){
+	var myName = prompt('Please enter your name');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = myHeading + myName;
+	
+	if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+	
+}
+
+
 var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
@@ -11,3 +28,7 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/firefox-icon.png');
     }
 }
+
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
