2ee359c283ca9d11681db89c57b7aaccfebce747
matszal <mszalkowski2011@gmail.com>
2017-11-15 14:21:30 +0000

    ne warchitecture diagram and previous work upload

--- NodeJsFiles/app.js
+++ NodeJsFiles/app.js
@@ -1,15 +1,21 @@
 const http = require('http');
-
+const fs = require('fs');
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) =>{
-	res.statusCode = 200;
-	res.setHeader('Content-type', 'text/plain');
-	res.end('Hello world');
+fs.readFile('index.html', (err, html) => {
+	if(err){
+		throw err;
+	}
 
-});
+	const server = http.createServer((req, res) =>{
+		res.statusCode = 200;
+		res.setHeader('Content-type', 'text/html');
+		res.write(html);
+		res.end();
+	});
 
-server.listen (port, hostname, () =>{
-	console.log('Sever started on port '+port);
+	server.listen (port, hostname, () =>{
+		console.log('Sever started on port '+port);
+	});
 });
