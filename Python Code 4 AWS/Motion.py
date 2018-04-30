ee2633409009f98aa6e79b8404d09ec72820a322
matszal <mszalkowski2011@gmail.com>
2018-04-30 22:15:19 +0100

    code clean up

--- Python Code 4 AWS/Motion.py
+++ Python Code 4 AWS/Motion.py
@@ -16,11 +16,9 @@ from time import sleep
 import sys
 import json
 
-
-# delay for temperature before sending data to database + sensor ID
-delay_temp          = 10
-sn_number           = '000001'
-led_status          = None
+# a time delay for temperature database store interval
+delay_temp          = 2
+heater_delay        = 1
 
 # read certificates required for authentication
 rootca = r'/home/pi/Desktop/pythonForAWS/certs/rootCA.pem'
@@ -28,10 +26,11 @@ certificate = r'/home/pi/Desktop/pythonForAWS/certs/certificate.pem.crt'
 keyfile = r'/home/pi/Desktop/pythonForAWS/certs/private.pem.key'
 hostName = open("/home/pi/Desktop/pythonForAWS/certs/hostName.txt", "r")
 
-
 # define gpio pins (board mode)
 pir_sensor          = 37
-led_test            = 35
+led_device          = 35
+led_heater          = 33
+
 # i2c bus of Pi 3
 i2c_bus             = 1
 # address devices on the i2c bus
@@ -47,16 +46,18 @@ mylcd = I2C_LCD_driver.lcd()
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pir_sensor, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(led_test, GPIO.OUT)
+GPIO.setup(led_device, GPIO.OUT)
+GPIO.setup(led_heater, GPIO.OUT)
+GPIO.output(led_heater, GPIO.LOW)
+
 # get camera instance
 camera  = picamera.PiCamera()
 
-
 # define IoT mqtt topics
 topic_iot           = "mytopic/iot"
-topic_iot2          = "mytopic/iot2"
 topic_led           = "mytopic/iot/led"
-topic_tmp           = "mytopic/iot/temp_read"
+topic_heat          = "mytopic/iot/heat"
+topic_tmp2          = "mytopic/iot/temp_read2"
 
 
 # setup AWS IoT
@@ -73,6 +74,8 @@ c = mqtt.Client()
 c.tls_set(rootca, certfile=certificate, keyfile=keyfile,
           cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
 
+
+
 # FUNCTIONS================================================================================================
 # calculate temperature based on the first word and the first byte
 def tmp_reading():
@@ -85,12 +88,16 @@ def tmp_reading():
             l4b = ( t_word & 0b1111000000000000)>>12
             temperature = ((t_byte<<4) | l4b) * 0.0625
             timestamp = datetime.datetime.now()
+            millis = time.time()
+
             print(' Temperature: {} C   Date: {} '. format(temperature, timestamp))
             mylcd.lcd_clear()
-            mylcd.lcd_display_string("Temp: %d %%" % temperature, 2)
-            msg = '"Date": "{}", "Device": "{:s}", "Temperature": "{}", "Loop": "{}"'.format(timestamp, sn_number, temperature, count)
+            mylcd.lcd_display_string("Temp: %.2f%% C" % temperature, 2)
+            #msg = '"Date": "{}", "Device": "{:s}", "Temperature": "{}", "Loop": "{}"'.format(timestamp, sn_number, temperature, count)
+            msg = '"Millis": "{}", "Temperature": "{}"'.format(millis, temperature)
             msg = '{'+msg+'}'
-            c.publish(topic_tmp, msg, 1)
+            #return temperature
+            c.publish(topic_tmp2, msg, 1)
             time.sleep(delay_temp)
 
     except KeyboardInterrupt:
@@ -101,96 +108,60 @@ def tmp_reading():
 def onc(c, userdfata, flags, rc):
     print("successfully connected to Amazon with RC ", rc)
     c.subscribe(topic_iot)
-    #c.subscribe(topic_iot2)
     c.subscribe(topic_led)
-    c.subscribe(topic_tmp)
+    c.subscribe(topic_tmp2)
+    c.subscribe(topic_heat)
 
 
 # wait for a message from aws console topic.
 def onm(c, userdata, msg):
     m = msg.payload.decode()
-    # print(m)
-
-    if is_json(m):
-        print("deal with json ")
-        j = json.loads(m)
-        temp = (j['Temperature'])
-        # temp = int(temp)
-        print(temp)
-        tempint = int(float(temp))
-        print(tempint)
-        if tempint < 20:
-            # led_status = True
-            GPIO.output(led_test, GPIO.HIGH)
-        else:
-            # led_status = False
-            GPIO.output(led_test, GPIO.LOW)
-
-
-    else:
-        print(m)
-        if m == 'hello':
-            # change topic here
-            c.publish(topic_iot, 'Hello from Python to Amazon')
-        elif m == 'on':
-            GPIO.output(led_test, GPIO.HIGH)
-        elif m == 'off':
-            GPIO.output(led_test, GPIO.LOW)
-
-"""
-
     print(m)
-    if m == 'hello':
-        c.publish(topic_iot, 'Hello from Python to Amazon')
+    if m.startswith("temperaturetoset"):
+        parse_temp = m.split(" ")[1]
+        temp_tresh = int(parse_temp)
+        print(temp_tresh)
+        heating_as_thread = Thread(target=tmp_reading2, args=[temp_tresh])
+        heating_as_thread.daemon = True
+        heating_as_thread.start()
+
     elif m == 'on':
-        GPIO.output(led_test, GPIO.HIGH)
+        GPIO.output(led_device, GPIO.HIGH)
     elif m == 'off':
-        GPIO.output(led_test, GPIO.LOW)
+        GPIO.output(led_device, GPIO.LOW)
 
-
+# run temperature reading on separate thread to enable heating controller
+def tmp_reading2(arg1):
     try:
-        json_object = json.loads(m)
-    except ValueError, e:
-        #pass # invalid json
-        print(m)
-        if m == 'hello':
-            c.publish(topic_iot, 'Hello from Python to Amazon')
-        elif m == 'on':
-            GPIO.output(led_test, GPIO.HIGH)
-        elif m == 'off':
-            GPIO.output(led_test, GPIO.LOW)
-        else:
-            pass # valid json
-            print("start parsing here.")
-            j = json.dumps(m)
-            #obj = json.parse(json_data)
-            #temperature = j.temperature
-            print j['Temperature']
-            #temp = int(j['Temperature'])
-            #print(temp)
-    """
+        while True:
+            max_temp = int(arg1)
 
-# function to detect if received message is in json format
-def is_json(my_msg):
-    try:
-        json_object = json.loads(my_msg)
-    except ValueError, e:
-        return False
-    return True
+            t_byte = dev_pi.i2c_read_byte_data(dev_tmp, 0)
+            t_word = dev_pi.i2c_read_word_data(dev_tmp, 0)
+            l4b = ( t_word & 0b1111000000000000)>>12
+            temperature = ((t_byte<<4) | l4b) * 0.0625
+            temp_int = int(temperature)
+
+            if temp_int <= max_temp:
+                GPIO.output(led_heater, GPIO.HIGH)
+            else:
+                GPIO.output(led_heater, GPIO.LOW)
+            time.sleep(heater_delay)
+    except KeyboardInterrupt:
+        print("Exitting")
 
 # function to detect interrupt event.
 def my_callback(channel):
     print("Event detected on pir sensor")
-    #sleep(4)
-    #take_snap()
+    take_snap()
 
 # function to take a snapshot and save in specific folder.
 # date_string is used to take a picture and add current date as a name to prevent image duplication.
 def take_snap():
     print("Taking snap\n")
-    date_string = time.strftime("%Y-%m-%d-%H:%M:%S")
+    date_string = time.strftime("%m-%d-%H-%M-%S")
     path        = '/home/pi/Desktop/Camera_test_python/pictures/'
-    date        = 'image_'+date_string
+    date        = 'image'+date_string
     ext         = '.jpg'
     full_path   = path+date+ext
 
@@ -222,19 +193,12 @@ background_thread.start()
 try:
 
     while True:
-        if GPIO.input(37) > 0.5:
-            print("\rMovement dtetected, isr triggered!")
-
-        else:
-            print("No movement")
+        # run this loop forever
         sleep(1)
 
 # if CTRL-C is pressed the main loop will break.
 except KeyboardInterrupt:
     print("Exitting")
-    #GPIO.cleanup()                          # reset ports
-
-
 
 finally:
     GPIO.remove_event_detect(pir_sensor)    # turn off event detect interrupt
@@ -243,4 +207,4 @@ finally:
     c.disconnect()
     r = dev_pi.i2c_close(dev_tmp)           # close i2c devices
     print("Connection terminated")
-#GPIO.cleanup()                          # reset ports
+    mylcd.lcd_clear()
